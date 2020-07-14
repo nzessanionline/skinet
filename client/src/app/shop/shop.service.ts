0b1e26d9aaed22f77345911987691c8436c50082
nzessanionline <nzessanionline@gmail.com>
2020-07-14 20:25:47 +0500

    End of Section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IBrand } from '../shared/models/brands';
 import { IType } from '../shared/models/productTypes';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -38,6 +39,11 @@ export class ShopService {
       })
     );
   }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
+  }
+
 
   getBrands() {
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
