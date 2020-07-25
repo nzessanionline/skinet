a1513adcc4034e84f13f39fdf54b6d7658261882
nzessanionline <nzessanionline@gmail.com>
2020-07-25 12:53:11 +0500

    End of Section 16

--- API/Extentions/SwaggerServiceExtentions.cs
+++ API/Extentions/SwaggerServiceExtentions.cs
@@ -8,9 +8,27 @@ namespace API.Extentions
     {
         public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
         {
-            services.AddSwaggerGen(c => 
+            services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "SkiNet API", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkiNet API", Version = "v1" });
+
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                 c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement {{securitySchema, new[] {"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirement);
             });
 
             return services;
@@ -19,8 +37,11 @@ namespace API.Extentions
         public static IApplicationBuilder UseSwaggerDocumention(this IApplicationBuilder app)
         {
             app.UseSwagger();
-            app.UseSwaggerUI(c => {c
-                .SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1");});
+            app.UseSwaggerUI(c =>
+            {
+                c
+.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1");
+            });
 
             return app;
         }
